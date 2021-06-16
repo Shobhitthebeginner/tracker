b5dbb2678cffae127f6d32d9487560286bba0f1b
Shobhitthebegineer <shobhit.18bcs1107@abes.ac.in>
2021-06-16 20:44:42 +0530

    Day4

--- lib/add task.dart
+++ lib/add task.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'dart:math';
 
 class AddTask extends StatefulWidget {
   @override
@@ -7,36 +9,77 @@ class AddTask extends StatefulWidget {
 
 class _AddTaskState extends State<AddTask> {
   @override
-
-
-    // Stroing all the dates till the last date
-    // since we have found the last date using generate
-    var listOfDates = new List<int>.generate(daysInMonth(DateTime(2020, 7)), (i) => i + 1);
-
-
-
-// this returns the last date of the month using DateTime
-
   Widget build(BuildContext context) {
+    var currDt = DateTime.now();
+    var listOfDay = new List.generate(
+        daysInMonth(currDt),
+            (i) =>
+        "${DateFormat('EEEE').format(currDt.add(Duration(days: i - 1)))}");
+    var listOfDates = new List.generate(
+        daysInMonth(currDt),
+            (i) =>
+        i+1);
+    final _random = Random();
+
     print(listOfDates);
+
     return Scaffold(
       body: ListView.builder(
-          itemCount: 31,
+          itemCount: listOfDates.length,
           itemBuilder: (BuildContext context, int index) {
-            return ListTile(
-                leading: Icon(Icons.list),
-                trailing: Text("Add Your Task",
-                  style: TextStyle(
-                      color: Colors.green, fontSize: 15),),
-                title: Text(" Day $index")
+            return Padding(
+
+              padding: EdgeInsets.all(10),
+              child: Container(
+                height: 40,
+                decoration: BoxDecoration(
+
+                    color: Colors.primaries[_random.nextInt(Colors.primaries.length)]
+                    [_random.nextInt(9) * 100],
+                    border: Border.all(color: Colors.black),
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(150.0),
+                        bottomLeft: Radius.circular(150.0),
+                    ),
+
+
+                ),
+                child: Row(
+
+                  children: [
+                      Expanded(
+                      flex: 4,
+                      child: Column(
+                        children: [
+                          Text("${listOfDates[index]}",textAlign: TextAlign.left),
+                          Text("${listOfDay[index]}",textAlign: TextAlign.left),
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      flex: 10,
+                      child: FlatButton(
+                        onPressed: (){},
+                        child: Text(
+                          "Add Your Task",
+                          textAlign: TextAlign.center,
+
+                          style: TextStyle(color: Colors.black, fontSize: 15),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             );
-          }
-      ),
+          }),
     );
   }
 }
-int daysInMonth(DateTime date){
+
+int daysInMonth(DateTime date) {
   var firstDayThisMonth = new DateTime(date.year, date.month, date.day);
-  var firstDayNextMonth = new DateTime(firstDayThisMonth.year, firstDayThisMonth.month + 1, firstDayThisMonth.day);
+  var firstDayNextMonth = new DateTime(firstDayThisMonth.year,
+      firstDayThisMonth.month + 1, firstDayThisMonth.day);
   return firstDayNextMonth.difference(firstDayThisMonth).inDays;
 }
